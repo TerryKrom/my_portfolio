b57cb6c6a575d254761bbfbe2b7c14872dc70d15
TerryKrom <terrykrom1@gmail.com>
2024-12-15 14:45:36 -0300

    Fix: removed unused comments in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,6 @@
-(function($) {
+(function ($) {
     'use strict';
 
-    var pluginName = 'ScrollIt',
-        pluginVersion = '1.0.3';
-
-    /*
-     * OPTIONS
-     */
     var defaults = {
         upKey: 38,
         downKey: 40,
@@ -14,29 +8,16 @@
         scrollTime: 600,
         activeClass: 'active',
         onPageChange: null,
-        topOffset : 0
+        topOffset: 0
     };
 
-    $.scrollIt = function(options) {
+    $.scrollIt = function (options) {
 
-        /*
-         * DECLARATIONS
-         */
         var settings = $.extend(defaults, options),
             active = 0,
             lastIndex = $('[data-scroll-index]:last').attr('data-scroll-index');
-
-        /*
-         * METHODS
-         */
-
-        /**
-         * navigate
-         *
-         * sets up navigation animation
-         */
-        var navigate = function(ndx) {
-            if(ndx < 0 || ndx > lastIndex) return;
+        var navigate = function (ndx) {
+            if (ndx < 0 || ndx > lastIndex) return;
 
             var targetTop = $('[data-scroll-index=' + ndx + ']').offset().top + settings.topOffset + 1;
             $('html,body').animate({
@@ -45,74 +26,51 @@
             }, settings.scrollTime);
         };
 
-        /**
-         * doScroll
-         *
-         * runs navigation() when criteria are met
-         */
         var doScroll = function (e) {
             var target = $(e.target).closest("[data-scroll-nav]").attr('data-scroll-nav') ||
-            $(e.target).closest("[data-scroll-goto]").attr('data-scroll-goto');
+                $(e.target).closest("[data-scroll-goto]").attr('data-scroll-goto');
             navigate(parseInt(target));
         };
 
-        /**
-         * keyNavigation
-         *
-         * sets up keyboard navigation behavior
-         */
         var keyNavigation = function (e) {
             var key = e.which;
-            if($('html,body').is(':animated') && (key == settings.upKey || key == settings.downKey)) {
+            if ($('html,body').is(':animated') && (key == settings.upKey || key == settings.downKey)) {
                 return false;
             }
-            if(key == settings.upKey && active > 0) {
+            if (key == settings.upKey && active > 0) {
                 navigate(parseInt(active) - 1);
                 return false;
-            } else if(key == settings.downKey && active < lastIndex) {
+            } else if (key == settings.downKey && active < lastIndex) {
                 navigate(parseInt(active) + 1);
                 return false;
             }
             return true;
         };
 
-        /**
-         * updateActive
-         *
-         * sets the currently active item
-         */
-        var updateActive = function(ndx) {
-            if(settings.onPageChange && ndx && (active != ndx)) settings.onPageChange(ndx);
+        var updateActive = function (ndx) {
+            if (settings.onPageChange && ndx && (active != ndx)) settings.onPageChange(ndx);
 
             active = ndx;
             $('[data-scroll-nav]').removeClass(settings.activeClass);
             $('[data-scroll-nav=' + ndx + ']').addClass(settings.activeClass);
         };
 
-        /**
-         * watchActive
-         *
-         * watches currently active item and updates accordingly
-         */
-        var watchActive = function() {
+        var watchActive = function () {
             var winTop = $(window).scrollTop();
 
-            var visible = $('[data-scroll-index]').filter(function(ndx, div) {
+            var visible = $('[data-scroll-index]').filter(function (ndx, div) {
                 return winTop >= $(div).offset().top + settings.topOffset &&
-                winTop < $(div).offset().top + (settings.topOffset) + $(div).outerHeight()
+                    winTop < $(div).offset().top + (settings.topOffset) + $(div).outerHeight()
             });
             var newActive = visible.first().attr('data-scroll-index');
             updateActive(newActive);
         };
 
-        /*
-         * runs methods
-         */
-        $(window).on('scroll',watchActive).scroll();
+        $(window).on('scroll', watchActive).scroll();
 
         $(window).on('keydown', keyNavigation);
 
-        $('body').on('click','[data-scroll-nav], [data-scroll-goto]', function(e){
+        $('body').on('click', '[data-scroll-nav], [data-scroll-goto]', function (e) {
             e.preventDefault();
             doScroll(e);
         });
@@ -120,11 +78,9 @@
     };
 }(jQuery));
 
-// Javascript main.js functions
-
-$(document).ready(function() {
-    $(window).on("scroll", function() {
-        if($(this).scrollTop() > 90) {
+$(document).ready(function () {
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 90) {
             $(".navbar").addClass("navbar-shrink");
         } else {
             $(".navbar").removeClass("navbar-shrink");
@@ -132,7 +88,7 @@ $(document).ready(function() {
     });
 
     function parallaxMouse() {
-        if($("#parallax").length) {
+        if ($("#parallax").length) {
             var scene = document.getElementById("parallax");
             var parallax = new Parallax(scene);
         }
@@ -140,70 +96,62 @@ $(document).ready(function() {
 
     parallaxMouse();
 
-    //skills meter
-
-    $(window).scroll(function(){
+    $(window).scroll(function () {
         var hT = $("#skill-bar-wrapper").offset().top;
         var hH = $("#skill-bar-wrapper").outerHeight();
         var wH = $(window).height();
         var wS = $(this).scrollTop();
 
-        if( wS > (hT + hH - 1.4 * wH)){
-            jQuery('.skillbar-container').each(function(){
+        if (wS > (hT + hH - 1.4 * wH)) {
+            jQuery('.skillbar-container').each(function () {
                 jQuery(this).find('.skills').animate({
-                    width:jQuery(this).attr('data-percent')
+                    width: jQuery(this).attr('data-percent')
                 }, 5000)
             })
         }
     })
 
-    //filter
-    ///enabling active button
     let $btns = $('.img-gallery .sortBtn .filter-btn');
-    $btns.click(function(e) {
+    $btns.click(function (e) {
         $('.img-gallery .sortBtn .filter-btn').removeClass('active');
         e.target.classList.add('active');
 
-    ///enabling filter selection according to the active button
         let selector = $(e.target).attr('data-filter');
         $('.img-gallery .grid').isotope({
-            filter:selector
+            filter: selector
         })
         return false;
     })
 
-    ///enabling gallery mode with magnific popup.js and maginif popup.css
     $('.image-popup').magnificPopup({
-        type:'image',
+        type: 'image',
         gallery: { enabled: true }
     })
 
-    // owl carousel
+
     $('.testimonial-slider').owlCarousel({
-        loop:true,
-        margin:0,
-        autoplay:true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
             },
-            600:{
-                items:2,
+            600: {
+                items: 2,
             },
-            1000:{
-                items:3,
+            1000: {
+                items: 3,
             }
         }
     })
 
-    //scrollit
     $.scrollIt({
-        topOffset:-50
+        topOffset: -50
     })
 
-    //Hiding Mobile Navbar when a nav link is clicked
-    $(".nav-link").on("click", function() {
+    $(".nav-link").on("click", function () {
         $(".navbar-collapse").collapse("hide");
     })
 
